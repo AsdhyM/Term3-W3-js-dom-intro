5931ded1a7128f2b00458c71e476687e5c365aae
AsdhyM <asdhyme@gmail.com>
2024-08-27 19:55:34 +1000

    feat: populate page with content

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -12,7 +12,27 @@ let dataArray = [
     "toyota corolla"
 ];
 
-dataArray.forEach(car => {
+
+let carsContainerElement = document.getElementById("carsContainer");
+let carsContainerList = document.createElement("ul");
+
+
+dataArray.forEach((car) => {
     console.log(car);
+
+    let newCarEntry = document.createElement("li");
+
+    newCarEntry.innerText += car;
+
+    carsContainerList.appendChild(newCarEntry);
 });
+
+
+carsContainerElement.appendChild(carsContainerList);
+
+
+
+
+
+
 
